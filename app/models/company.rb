7db1f4976b55306a0a2aa16656ec668d6ae3f838
Leonid Medovyy <leonid@logik.works>
2021-02-05 10:33:26 -0700

    added fields to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Company < ApplicationRecord
-  jsonb_accessor :fields
+  jsonb_accessor :fields,
+                 slug: :string,
+                 url: :string
 end
