ab5b4783f8202c7eb992e426d6c6cf01dbc550d0
Leonid Medovyy <leonid@logik.works>
2021-02-05 12:58:42 -0700

    added administrate and create an import script

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,9 +5,14 @@ class Company < ApplicationRecord
                   slug: :string,
                   url: :string,
                   keyword: :string,
+                  headquarters: :string,
                   employees: :string,
                   revenue: :string,
+                  status: :string,
                   ipo: :string,
                   website_tech: :string,
                   company_tech: :string
+
+  # we don't want to have duplicate names
+  validates_uniqueness_of :name
 end
