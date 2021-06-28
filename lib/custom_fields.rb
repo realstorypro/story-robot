c044517b816a3d2eb86df7318dfc2128a5c611b5
Leonid Medovyy <leonid@storypro.io>
2021-06-28 16:11:12 -0700

    added more fields to custom fields

--- lib/custom_fields.rb
+++ lib/custom_fields.rb
@@ -3,11 +3,15 @@ class CustomFields
     # custom.cf_6evh1292hlGTL0FqvkAXb3ROE6JBX1ZmqADApRpAndh - Decision Maker [Contact] w/ Yes & No values
     # custom.cf_iuK23d7LKjVFuR9z52ddWRHEjCkkHZ23xCRzLvGIP83 - Excluded from email sequence [Contact] w/ Yes & No values
     # custom.cf_He1yXt17QGicc0ZXLm5c813K69CNcKpBqBisz84t1HN - Available decision makers [Lead] (integer)
+    # custom.cf_KhipBANwlul9s4rxq1r0Hpw2lOekg4Q73EvyXj46IDQ - Technology [Lead] (choice-single)
+    # custom.cf_l85fqSolodDAbcfiHDqxGb9fVvsrnQhaMem3VhoPexO - Frequency [Lead] (multiple-choice)
 
     @fields = {
       decision_maker: 'custom.cf_6evh1292hlGTL0FqvkAXb3ROE6JBX1ZmqADApRpAndh',
       excluded_from_sequence: 'custom.cf_iuK23d7LKjVFuR9z52ddWRHEjCkkHZ23xCRzLvGIP83',
-      available_decision_makers: 'custom.cf_He1yXt17QGicc0ZXLm5c813K69CNcKpBqBisz84t1HN'
+      available_decision_makers: 'custom.cf_He1yXt17QGicc0ZXLm5c813K69CNcKpBqBisz84t1HN',
+      technology: 'custom.cf_KhipBANwlul9s4rxq1r0Hpw2lOekg4Q73EvyXj46IDQ',
+      frequency: 'custom.cf_l85fqSolodDAbcfiHDqxGb9fVvsrnQhaMem3VhoPexO'
     }
   end
 
